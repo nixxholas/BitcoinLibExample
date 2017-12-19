28fa1b02b39344f98dad782e49db14625b3c99bb
nixxholas <nicholas@slyck.net>
2017-12-19 17:24:27 +0800

    Source: Initial Commit
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ BitcoinLibExample/ApplicationSettings.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+
+namespace BitcoinLibExample
+{
+    /// <summary>
+    /// A wrapper class for the Microsoft.Extensions.Configuration class
+    /// that serves as a convenient way to acquire settings off appsettings.json
+    /// </summary>
+    public class ApplicationSettings
+    {
+        private static Dictionary<string, string> DataCache = new Dictionary<string, string>();
+
+        #region Protected Settings Init
+        private static bool IsProtectedSettingsLoaded = false;
+        private static Object IsProtectedSettingsLoadedLock = new Object();
+
+        /// <summary>
+        /// Load application api/keys from sitekeyconfig.json and stores it in memory-encrypted.
+        /// </summary>
+        public static void InitializeProtectedAppSettings()
+        {
+            lock (IsProtectedSettingsLoadedLock)
+            {
+                if (!IsProtectedSettingsLoaded)
+                {
+                    IsProtectedSettingsLoaded = true;
+
+                    const string resourceName = "config.json";
+
+                    Assembly assembly = Assembly.GetExecutingAssembly();
+                    string codeBase = assembly.CodeBase;
+                    UriBuilder uri = new UriBuilder(codeBase);
+                    string path = Path.Combine((Path.GetDirectoryName(uri.Path)), resourceName);
+
+                    using (Stream stream = new FileStream(path, FileMode.OpenOrCreate))
+                    {
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            JObject JsonObject = JObject.Parse(reader.ReadToEnd());
+
+                            RecursivelyLoadJsonSettings(JsonObject, null);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void RecursivelyLoadJsonSettings(JObject JsonObject, JProperty JsonProperty)
+        {
+            foreach (JToken token in JsonProperty == null ? JsonObject.Children() : JsonProperty.Children())
+            {
+                if (token.Type == JTokenType.Property)
+                {
+                    RecursivelyLoadJsonSettings(null, (JProperty)token);
+                }
+                else if (token.Type == JTokenType.Object)
+                {
+                    RecursivelyLoadJsonSettings((JObject)token, null);
+                }
+                else
+                {
+                    if (token.Type == JTokenType.String)
+                    {
+                        string path = token.Path.Replace(".", "/");
+                        string value = (string)token;
+
+                        DataCache.Add(SHA256Hash.Hash(path), AES256Hash.EncryptString(path, value)); // encrypt with real path independent of the hashed path key.
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets an encrypted string settings from the sitekeyconfig.json file.
+        /// 
+        /// Prevents memory based attacks to extract any sensitive keys in cache.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="defaultfallback">The default value if it does not exist</param>
+        /// <returns></returns>
+        public static string GetEncryptedString(string path, string defaultfallback = "")
+        {
+            InitializeProtectedAppSettings();
+
+            // Check cache
+            string hashedPath = SHA256Hash.Hash(path);
+            if (DataCache.ContainsKey(hashedPath))
+            {
+                return AES256Hash.DecryptString(path, DataCache[hashedPath]); // decrypt with real path independent of the hashed path key.
+            }
+            return null;
+        }
+        #endregion
+    }
+}
---
+++ BitcoinLibExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using BitcoinLib.Services.Coins.Bitcoin;
+using Newtonsoft.Json.Linq;
+
+namespace BitcoinLibExample
+{
+    class Program
+    {
+        private static IBitcoinService _BitcoinManager;
+        public static IBitcoinService BitcoinManager
+        {
+            get { return _BitcoinManager; }
+            private set { }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Setting up!!");
+
+            // Bitcoind
+            _BitcoinManager = new BitcoinService(ApplicationSettings.GetEncryptedString("BTCNodeDaemonURL"),
+                                                 ApplicationSettings.GetEncryptedString("BTCNodeJsonRPCUser"),
+                                                 ApplicationSettings.GetEncryptedString("BTCNodeJsonRPCPass"),
+                                                "", 30); // Wallet passphrase will be put up later..
+
+            Console.WriteLine("Testing BTC Service...");
+
+            Console.WriteLine("Bitcoin Difficulty: " + _BitcoinManager.GetDifficulty());
+            //Console.WriteLine("" + _BitcoinManager.GetNetworkInfo().LocalServices);
+
+            while (true) {
+                Console.WriteLine();
+                // Here's the prepared hash = de6605800010593850355721072bcc44bd2c5683a111a615c41aad3a05b8ac70
+                //9c9dd22fc1a0222be84a756a19f8e1729941886b7b7e47f9eeb65780dbc88c62
+                Console.WriteLine("Enter a raw transaction: ");
+                string input = Console.ReadLine();
+
+                if (input.Equals("quit")) {
+                    Environment.Exit(0);
+                }
+
+                var rawTxResp = _BitcoinManager.GetRawTransaction(input);
+                Console.WriteLine(JObject.FromObject(rawTxResp));
+            }
+        }
+    }
+}
